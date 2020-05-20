f909c1fe813efbce27fcf4a3db2b31640f290e67
Maddie Nisbet <maddienisbet5@gmail.com>
2020-05-20 12:26:08 +0700

    add map display for attractions

--- travel_app/src/visualization/first_app.py
+++ travel_app/src/visualization/first_app.py
@@ -5,10 +5,10 @@ import helper as hp
 
 st.title("Travel App")
 
-df = pd.read_csv('/Users/tristannisbet/Documents/SM/Dataframe/all_restaurants2.csv')
+df2 = pd.read_csv('/Users/tristannisbet/Documents/SM/Dataframe/all_restaurants2.csv')
 
 st.write("Here's the first table")
-st.write(df)
+st.write(df2)
 
 
 
@@ -28,13 +28,23 @@ st.write('Values:', values)
 
 
 @st.cache
-def load_data():
-    return pd.read_csv("/Users/tristannisbet/Documents/SM/Dataframe/attractions_count.csv")
+def load_data_attraction(file_path):
+    attraction_data_new = pd.read_csv(file_path, index_col=[0])
+    attraction_data_new.rename(columns={'geometry.location.lat': 'latitude', 'geometry.location.lng': 'longitude'}, inplace=True)
+    attraction_data_new.set_index(['city', 'country'], inplace=True)
+    return attraction_data_new
+
+def load_data(file_path):
+	return pd.read_csv(file_path, index_col=[0])
 
 
-data = load_data()
 
-st.dataframe(data)
+
+data = load_data("/Users/tristannisbet/Documents/SM/Dataframe/attractions_count.csv")
+
+attraction_data = load_data_attraction("/Users/tristannisbet/Documents/SM/Dataframe/all_attractions.csv")
+
+
 
 st.write("What type of tourist attraction are you most interested in?")
 attraction_type_selected = st.radio("Attraction Type", ['History', 
@@ -47,13 +57,29 @@ def which_city_attraction(radio_choice):
 		city = data.sort_values(type_to_df_dict[radio_choice], ascending=False).reset_index(drop=True).city[0]
 		return city
 
-def city_output(city):
-	st.write('You should visit ', city)
+
+def display_map(city):
+	df = attraction_data.loc[city].copy()
+	st.write("You should go to ", city)
+	st.map(df, zoom=0)
+
 
 # Funciton call
-city_output(which_city_attraction(attraction_type_selected))
+display_map(which_city_attraction(attraction_type_selected))
 
 
+
+
+# st.write("This is the attraction data for each city")
+# st.write(attraction_data)
+
+# st.title("Map")
+
+# bk = attraction_data.set_index(['country', 'city']).loc['Thailand']
+
+# bk.rename(columns={'geometry.location.lat': 'latitude', 'geometry.location.lng': 'longitude'}, inplace=True)
+
+# st.map(bk)
 
 
 
