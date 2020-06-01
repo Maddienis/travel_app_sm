945c390e31edf68c7202c0d773e094217bd69dd6
Maddie Nisbet <maddienisbet5@gmail.com>
2020-06-01 15:47:26 +0700

    add rename columns after creating df

--- travel_app/src/features/raw_to_df.py
+++ travel_app/src/features/raw_to_df.py
@@ -1,18 +1,9 @@
 import pandas as pd 
 import numpy as np 
+import os
 
 
-def load_raw_df(places_df):
-	raw_df = places_df
 
-	return raw_df
-
-def add_country_city(raw_df):
-	code = raw_data['plus_code.compound_code'][0]
-	split_code = code.split()
-	country =  
-	city = split_code[1]
-	raw_df['country'], raw_df['city'] = [country, city]
 
 def column_selection(df):
 	cols_to_keep = ['country', 'city', 'name', 'formatted_address',
@@ -20,10 +11,15 @@ def column_selection(df):
 	'geometry.location.lat', 'geometry.location.lng', 'place_id']
 
 	df = df[cols_to_keep]
+
+	df.rename(columns={'formatted_address': 'address',
+		'geometry.location.lat': 'latitude', 'geometry.location.lng': 'longitude'})
 	return df
 
-
 	
+
+
+
 
 
 
