9631be50557055305f57a5e92ab609e9968203e2
Maddie Nisbet <maddienisbet5@gmail.com>
2020-06-01 15:48:44 +0700

    add venues to database function

--- db_connect.py
+++ db_connect.py
@@ -8,11 +8,21 @@ except Exception as e:
 	print('Error durring connection: ', str(e))
 
 
-results = conn.execute("SELECT name FROM all_food")
+results = conn.execute("SELECT name FROM all_food LIMIT 3")
 
 for row in results:
 	print(row)
 
+
+
+def add_venues(conn, df):
+	sql = ''' INSERT INTO all_food(country, city, name, address, price_level,
+	rating, user_ratings_total, types, latitude, longitude, place_id)
+	VALUES(?,?,?,?,?,?,?,?,?,?,?) '''
+
+	conn.executemany(sql, df.to_records(index=False))
+	conn.commit
+	return 
 
 conn.close()
 
