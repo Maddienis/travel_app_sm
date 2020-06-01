1826fab5303e195dd1121cc6ebef1433a7fa49f3
Maddie Nisbet <maddienisbet5@gmail.com>
2020-06-01 15:48:09 +0700

    pass city through functions to create new column

--- travel_app/src/data/api_functions.py
+++ travel_app/src/data/api_functions.py
@@ -16,7 +16,7 @@ def build_query(city_list, query_list):
   for city in city_list:
       for query in query_list:
           url = base_url.format(key) + '&query={}'.format(query.format(city))
-          data = find_places_api(url)
+          data = find_places_api(url, city)
 
   return data
 
@@ -24,7 +24,7 @@ def build_query(city_list, query_list):
 def make_next_url(results, base_url):
   return base_url.format(key) + '&pagetoken={}'.format(results['next_page_token'])
 
-def find_places_api(url, data=[]):
+def find_places_api(url, city, data=[]):
   print(url)
   results = requests.get(url).json()
   time.sleep(5)
@@ -33,16 +33,18 @@ def find_places_api(url, data=[]):
   print(results['status'])
   if 'next_page_token' not in results.keys() and results['status']=='OK':
     print('no next')
-    return create_df(data)
+    return create_df(data, city)
   elif results['status']=='OK':
     print('next')
     new_url = make_next_url(results, base_url=base_url)
-    return find_places_api(new_url, data)
+    return find_places_api(new_url, city, data)
   else:
-    return create_df(data)
+    return create_df(data, city)
 
-def create_df(json_data):
+def create_df(json_data, city):
   df = pd.json_normalize(json_data)
+  df['city'] = city
+  # Add id, and country also.
   # ADD TO TABLE??
   return df
 
