d311dab36876a6170f2f7a7679c2e0b667b970a0
Maddie Nisbet <maddienisbet5@gmail.com>
2020-06-04 14:26:04 +0700

    add function to retrieve city df from database

--- src/data/db_connect.py
+++ src/data/db_connect.py
@@ -1,4 +1,6 @@
 import sqlite3
+import pandas as pd
+
 
 try:
 	conn = sqlite3.connect('places_data.db')
@@ -17,7 +19,14 @@ for row in results:
 
 def write_db(table_name, df, conn):
     df.to_sql(table_name, conn, if_exists="append")
+    return 
 
+
+
+def retrieve_city(table_name, conn):
+	df = pd.read_sql_query("select * from table_name;", conn)
+
+	return df
 
 conn.close()
 
