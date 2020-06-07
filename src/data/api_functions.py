f0e349a5eebb36934543a44567169ff18d66df48
Maddie Nisbet <maddienisbet5@gmail.com>
2020-06-07 13:21:43 +0700

    working pipeline in python files with test db

--- src/data/api_functions.py
+++ src/data/api_functions.py
@@ -1,60 +1,75 @@
 import pandas as pd
 import numpy as np
 import os
-
+import json, requests
+import time
 
 key = os.getenv('G_API_KEY')
 base_url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?language=en&key={}'
-
-
 query_dict = {'attractions': "{}+points+of+interest",
-  'restaurants': "best+restaurants+in+{}",
-  'restaurants_price': 'best+restaurants+in+{}&minprice={}&maxprice={}'}
-
-city_list = ['Portland', 'Krabi']
-
-
-
-def build_query(city, query):
-  if 'maxprice' in query:
-    k = 1
-    while k <= 4:
-      url = base_url.format(key) + '&query={}'.format(query.format(city, k, k))
-      k += 1 
-      return url
-            # DO I need a return for each k? 
-    else:
-        url = base_url.format(key) + '&query={}'.format(query.format(city))
-
-  return url
+              'restaurants': "best+restaurants+in+{}",
+             'restaurants_one': 'best+restaurants+in+{}&minprice=1&maxprice=1',
+             'restaurants_two': 'best+restaurants+in+{}&minprice=2&maxprice=2',
+             'restaurants_three': 'best+restaurants+in+{}&minprice=3&maxprice=3',
+             'restaurants_four': 'best+restaurants+in+{}&minprice=4&maxprice=4'}
 
 
-def make_next_url(results, base_url):
-  return base_url.format(key) + '&pagetoken={}'.format(results['next_page_token'])
+
+def build_url(city, query, country):
+    base_url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?language=en&key={}'
+    cc = [city, country]
+    cc = '+'.join(cc)
+    url = base_url.format(key) + '&query={}'.format(query.format(cc))
+    return url
+
+
+def make_next_page_url(results, base_url):
+    return base_url.format(key) + '&pagetoken={}'.format(results['next_page_token'])
+
 
 def find_places_api(url, data=[]):
-  print(url)
-  results = requests.get(url).json()
-  time.sleep(3)
-  data = data + results['results']
-  print(results.keys())
-  print(results['status'])
-  if 'next_page_token' not in results.keys() and results['status']=='OK':
-    print('no next')
-    return data
-  elif results['status']=='OK':
-    print('next')
-    new_url = make_next_url(results, base_url=base_url)
-    return find_places_api(new_url, data)
-  else:
-    return data 
+    results = requests.get(url).json()
+    time.sleep(3)
+    data = data + results['results']
+    print(results.keys())
+    print(results['status'])
+    if 'next_page_token' not in results.keys() and results['status']=='OK':
+        print('no next')
+        return data
+    elif results['status']=='OK':
+        print('next')
+        new_url = make_next_page_url(results, base_url=base_url)
+        return find_places_api(new_url, data)
+    else:
+        return data 
 
-def create_df(json_data, city):
-  #find_places_api()
-  df = pd.json_normalize(json_data)
-  city.replace('+', ' ')
-  df['city'] = city
-  df = column_selection(df)
-  return df
 
+def create_df(json_data, city, country, id, table_name):
+    df = pd.json_normalize(json_data)
+    df['city'] = city.replace('+', ' ')
+    df['country'] = country.replace('+', ' ')
+    df['id'] = id
+    df = column_selection(df, table_name)
+    return df
+
+
+def column_selection(df, table_name):
+    if table_name == 'attractions':
+        cols_to_keep = ['country', 'city', 'name', 'formatted_address',
+                        'rating', 'user_ratings_total', 'types',
+                        'geometry.location.lat', 'geometry.location.lng', 'place_id', 'id']
+    else:
+        cols_to_keep = ['country', 'city', 'name', 'formatted_address', 'price_level',
+                        'rating', 'user_ratings_total', 'types',
+                        'geometry.location.lat', 'geometry.location.lng', 'place_id', 'id']
     
+    df = df[cols_to_keep].copy()
+    df.rename(columns={'formatted_address': 'address',
+                       'geometry.location.lat': 'latitude',
+                       'geometry.location.lng': 'longitude'}, inplace=True)
+    df['types'] = df.types.astype(str)
+    
+    return df
+
+
+ 
--- src/data/db_connect.py
+++ src/data/db_connect.py
@@ -1,32 +1,38 @@
 import sqlite3
 import pandas as pd
 
-
 try:
-	conn = sqlite3.connect('places_data.db')
+	conn = sqlite3.connect('/Users/tristannisbet/Documents/travel_app/notebooks/test.db')
 	print('Opened database')
 
 except Exception as e:
 	print('Error durring connection: ', str(e))
 
 
-results = conn.execute("SELECT name FROM all_food LIMIT 3")
+# Where does my connnection function have to be?
+# Change db 
+def write_db(table_name, df):
+    try:
+        conn = sqlite3.connect('/Users/tristannisbet/Documents/travel_app/notebooks/test.db')
 
-for row in results:
-	print(row)
+    except Exception as e:
+        print('Error durring connection: ', str(e))
 
+    try:
+        df.to_sql(table_name, con=conn, if_exists="append", index=False)
+        
+    except sqlite3.DatabaseError as er:
+        print('er:', er.message)
 
-
-def write_db(table_name, df, conn):
-    df.to_sql(table_name, conn, if_exists="append")
     return 
 
 
+def get_city():
+	city_df = pd.read_sql_query("select * from city_country limit 3;", conn)
 
-def retrieve_city(table_name, conn):
-	df = pd.read_sql_query("select * from table_name;", conn)
+	return city_df
 
-	return df
-
-conn.close()
+df = get_city()
+print(df)
+#conn.close()
 
