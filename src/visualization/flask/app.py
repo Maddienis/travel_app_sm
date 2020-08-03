41e8ecbf96006f0217f724ea2c5a71fcb6fe91e7
Maddie Nisbet <maddienisbet5@gmail.com>
2020-08-03 15:34:15 +0700

    re initialize bootstrap with downloaded files instead of CDN

--- src/visualization/flask/app.py
+++ src/visualization/flask/app.py
@@ -10,11 +10,3 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	return render_template('index.html')
-
-
-# from flask import render_template
-
-# @app.route('/hello/')
-# @app.route('/hello/<name>')
-# def hello(name=None):
-#     return render_template('hello.html', name=name)
